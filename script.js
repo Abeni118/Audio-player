4266f109f5b5caeecb93d5fc87fd2dc58f54aeb0
Abeni118 <abenezertekalign118@gmail.com>
2025-11-20 23:51:45 +0300

    commit

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 const image=document.querySelector('img');
 const title=document.getElementById('title')
-const artist=document.getElementById('artst')
+const artist=document.getElementById('artist')
 const music=document.querySelector('audio');
 const prevBtn=document.getElementById('prev');
 const playBtn=document.getElementById('play');
 const nextBtn=document.getElementById('next');
 
 // creat array for each of music 
-const songs=[
-{
-   name:'ለምን ጠላኸኝ ወንድሜ ' ,
-   displayname:'Orthodox Mezmur',
-   artist:'Mirtinesh',
-},
-{
-    name:'ለምን ጠላኸኝ ወንድሜ ',
-    displayname:'Ethiopian mezmur',
-    artist:'Tewdros Yosef'
-},
-{
-    name:'ታላቅ በሆነው ',
-    displayname:'Ethiopian mezmur',
-    artist:'sister Lidya'
-},
-{
-    name:'ይላል አንደበቴ ',
-    displayname:'Ethiopian mezmur',
-    artist:'Tewedros Yosef'
-},
+const songs = [
+  {
+    name: 'mezmur-1',
+    displayName: 'ምክንያት ስላለኝ ነው',
+    artist: 'Mirtinesh',
+    image: "image-1"
+  },
+  {
+    name: 'mezmur-2',
+    displayName: 'ለምን ጠላኸኝ ወንድሜ',
+    artist: 'Tewdros Yosef',
+    image: "image-2"
+  },
+  {
+    name: 'mezmur-3',
+    displayName: 'ታላቅ በሆነው',
+    artist: 'Sister Lidya',
+    image: "image-3"
+  },
+  {
+    name: 'mezmur-4',
+    displayName: 'ይላል አንደበቴ',
+    artist: 'Tewedros Yosef',
+    image: "image-4"
+  }
 ];
 
 let isPlaying=false;
 
 function playSong(){
     isPlaying=true;
+        playBtn.classList.replace('fa-play', 'fa-pause');
     music.play();
 }
 function pauseSong(){
     isPlaying=false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Pause');
     music.pauseSong();
 }
 
@@ -46,4 +53,36 @@ function pauseSong(){
 playBtn.addEventListener("click", function () {
   isPlaying ? pauseSong() : playSong();
 });
-//update
+//update dom 
+function loadSong(song){
+    title.textContent=song.displayName;
+    artist.textContent=song.artist;
+    music.src=`music/${song.name}.mp3`;
+    image.src=`img/${song.image}.jpg` ;
+}
+//current song 
+let songIndex = 0;
+
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+loadSong(songs[songIndex]);
+// event listner
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
